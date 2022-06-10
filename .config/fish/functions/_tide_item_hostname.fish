c5afd354fde61b14feeec01bdffbb8b2c6e393bf
Keltirion <llonczynski@gmail.com>
2022-06-10 19:55:16 +0000

    Repository cleanup

--- .config/fish/functions/_tide_item_hostname.fish
+++
@@ -1,8 +0,0 @@
-function _tide_item_hostname
-
-  _tide_print_item hostname $tide_example_icon' ' (cat /etc/hostname)
-end
-
-funcsave _tide_item_hostname
-
-set --append tide_right_prompt_items hostname
