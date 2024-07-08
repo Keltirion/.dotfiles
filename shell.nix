4c4fc74e1e5bb26fbf563406591ccee6f3f17899
Keltrion <llonczynski@gmail.com>
2024-07-08 12:15:15 +0200

    Initial shell.nix file

---
+++ shell.nix
@@ -0,0 +1,18 @@
+# shell.nix
+
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+		zsh
+		kubectl
+  ];
+
+	shellHook = ''
+		zsh
+	'';
+}
+
