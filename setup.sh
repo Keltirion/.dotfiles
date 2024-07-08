410c5fcb01109a16565bbebbada0a713a5420612
Keltrion <llonczynski@gmail.com>
2024-07-08 11:53:10 +0200

    Fixes

--- setup.sh
+++ setup.sh
@@ -5,12 +5,15 @@ sudo apt-get -q -y install zsh
 export ZDOTDIR=$HOME/.config/zsh
 export ZSH=$HOME/.config/zsh/.oh-my-zsh/
 export RUNZSH=no
+export CHSH=no
 export KEEP_ZSHRC=yes
 
-ln -s $HOME/repositories/personal/dotfiles/.config/zsh $HOME/.config/zsh
-ln -s $HOME/repositories/personal/dotfiles/.config/starship.toml $HOME/.config/starship.toml
-ln -s $HOME/repositories/personal/dotfiles/.config/nvim $HOME/.config/nvim
+ln -s $HOME/repositories/personal/dotfiles/.config/zsh $HOME/.config/
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+ln -s $HOME/repositories/personal/dotfiles/.config/starship.toml $HOME/.config/starship.toml
+ln -s $HOME/repositories/personal/dotfiles/.config/nvim $HOME/.config/
+
 
 curl -sS https://starship.rs/install.sh | sh
