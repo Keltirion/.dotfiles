c663ed94e0a4d388466c11316e0200f381dde650
Keltrion <llonczynski@gmail.com>
2024-07-10 14:26:44 +0200

    Removed unused configs

--- .config/yabai/scripts/find-display.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tabletSpace=$(yabai -m query --displays | jq --arg v "$1" '.[] | select(.uuid==$v)' | jq '.spaces[0]')
-
-if [ ! -z "$tabletSpace" ]
-then
-  yabai -m space "$tabletSpace" --label $2
-fi
