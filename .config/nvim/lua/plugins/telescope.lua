e67ba21076eeb5e42a5c18e67fa5e52aec1c9b96
Keltrion <llonczynski@gmail.com>
2024-09-11 11:27:19 +0200

    Removed telescope extensions

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,8 +3,6 @@ return {
 	version = '0.1.5',
 	dependencies = {
 		'nvim-lua/plenary.nvim',
-		'nvim-telescope/telescope-project.nvim',
-		'cljoly/telescope-repo.nvim',
 	},
 	config = function()
 		local builtin = require('telescope.builtin')
@@ -25,26 +23,12 @@ return {
 						["<C-q>"] = actions.close
 					}
 				},
-			},
-			extensions = {
-				repo = {
-					list = {
-						fd_opts = {
-							"--no-ignore-vcs",
-						},
-						search_dirs = {
-							"~/Repositories/",
-						},
-					},
-				},
-			},
+			}
 		}
 
 		vim.keymap.set('n', '<leader>ps', function()
 			builtin.grep_string({ search = vim.fn.input("grep > ") });
 		end)
 
-		require 'telescope'.load_extension 'project'
-		require 'telescope'.load_extension 'repo'
 	end
 }
