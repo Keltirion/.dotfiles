43faf17b7f392274d84df47e6536bd2ab80fd366
Keltrion <llonczynski@gmail.com>
2024-09-11 11:28:00 +0200

    Wezterm config upgrade

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -23,7 +23,7 @@ config.window_background_opacity = 1.0
 config.window_decorations = 'RESIZE'
 
 -- Tab bar
-config.use_fancy_tab_bar = false
+config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_max_width = 25
 config.show_tab_index_in_tab_bar = true
@@ -36,136 +36,149 @@ config.cursor_blink_ease_in = "Linear"
 config.cursor_blink_ease_out = "Linear"
 
 local process_icons = {
-	['ssh'] = {
-		{ Text = wezterm.nerdfonts.md_ssh }
-	},
-  ['docker'] = {
-    { Text = wezterm.nerdfonts.linux_docker },
-  },
-  ['docker-compose'] = {
-    { Text = wezterm.nerdfonts.linux_docker },
-  },
-  ['kuberlr'] = {
-    { Text = wezterm.nerdfonts.linux_docker },
-  },
-  ['kubectl'] = {
-    { Text = wezterm.nerdfonts.md_kubernetes },
-  },
-  ['k9s'] = {
-    { Text = wezterm.nerdfonts.md_kubernetes },
-  },
-  ['terraform'] = {
-    { Text = wezterm.nerdfonts.md_terraform },
-  },
-  ['terragrunt'] = {
-    { Text = wezterm.nerdfonts.md_terraform },
-  },
-  ['nvim'] = {
-    { Text = wezterm.nerdfonts.custom_neovim },
-  },
-  ['neovim'] = {
-    { Text = wezterm.nerdfonts.custom_neovim },
-  },
-  ['vim'] = {
-    { Text = wezterm.nerdfonts.dev_vim },
-  },
-  ['lazygit'] = {
-    { Text = wezterm.nerdfonts.cod_git_pull_request },
-  },
-  ['node'] = {
-    { Text = wezterm.nerdfonts.mdi_hexagon },
-  },
-  ['zsh'] = {
-    { Text = wezterm.nerdfonts.cod_terminal },
-  },
-  ['bash'] = {
-    { Text = wezterm.nerdfonts.cod_terminal_bash },
-  },
-  ['btm'] = {
-    { Text = wezterm.nerdfonts.mdi_chart_donut_variant },
-  },
-  ['htop'] = {
-    { Text = wezterm.nerdfonts.mdi_chart_donut_variant },
-  },
-  ['cargo'] = {
-    { Text = wezterm.nerdfonts.dev_rust },
-  },
-  ['go'] = {
-    { Text = wezterm.nerdfonts.mdi_language_go },
-  },
-  ['lazydocker'] = {
-    { Text = wezterm.nerdfonts.linux_docker },
-  },
-  ['git'] = {
-    { Text = wezterm.nerdfonts.dev_git },
-  },
-  ['lua'] = {
-    { Text = wezterm.nerdfonts.seti_lua },
-  },
-  ['wget'] = {
-    { Text = wezterm.nerdfonts.mdi_arrow_down_box },
-  },
-  ['curl'] = {
-    { Text = wezterm.nerdfonts.mdi_flattr },
-  },
-  ['gh'] = {
-    { Text = wezterm.nerdfonts.dev_github_badge },
-  },
+	['ssh'] = { {
+		Text = wezterm.nerdfonts.md_ssh
+	} },
+	['docker'] = { {
+		Text = wezterm.nerdfonts.linux_docker
+	} },
+	['docker-compose'] = { {
+		Text = wezterm.nerdfonts.linux_docker
+	} },
+	['kuberlr'] = { {
+		Text = wezterm.nerdfonts.linux_docker
+	} },
+	['kubectl'] = { {
+		Text = wezterm.nerdfonts.md_kubernetes
+	} },
+	['k9s'] = { {
+		Text = wezterm.nerdfonts.md_kubernetes
+	} },
+	['terraform'] = { {
+		Text = wezterm.nerdfonts.md_terraform
+	} },
+	['terragrunt'] = { {
+		Text = wezterm.nerdfonts.md_terraform
+	} },
+	['nvim'] = { {
+		Text = wezterm.nerdfonts.custom_neovim
+	} },
+	['neovim'] = { {
+		Text = wezterm.nerdfonts.custom_neovim
+	} },
+	['vim'] = { {
+		Text = wezterm.nerdfonts.dev_vim
+	} },
+	['lazygit'] = { {
+		Text = wezterm.nerdfonts.cod_git_pull_request
+	} },
+	['node'] = { {
+		Text = wezterm.nerdfonts.mdi_hexagon
+	} },
+	['zsh'] = { {
+		Text = wezterm.nerdfonts.cod_terminal
+	} },
+	['bash'] = { {
+		Text = wezterm.nerdfonts.cod_terminal_bash
+	} },
+	['btm'] = { {
+		Text = wezterm.nerdfonts.mdi_chart_donut_variant
+	} },
+	['htop'] = { {
+		Text = wezterm.nerdfonts.mdi_chart_donut_variant
+	} },
+	['cargo'] = { {
+		Text = wezterm.nerdfonts.dev_rust
+	} },
+	['go'] = { {
+		Text = wezterm.nerdfonts.mdi_language_go
+	} },
+	['lazydocker'] = { {
+		Text = wezterm.nerdfonts.linux_docker
+	} },
+	['git'] = { {
+		Text = wezterm.nerdfonts.dev_git
+	} },
+	['lua'] = { {
+		Text = wezterm.nerdfonts.seti_lua
+	} },
+	['wget'] = { {
+		Text = wezterm.nerdfonts.mdi_arrow_down_box
+	} },
+	['curl'] = { {
+		Text = wezterm.nerdfonts.mdi_flattr
+	} },
+	['gh'] = { {
+		Text = wezterm.nerdfonts.dev_github_badge
+	} }
 }
 
 local function get_current_working_dir(tab)
-  local current_dir = tab.active_pane.current_working_dir
-  local HOME_DIR = string.format('file://%s', os.getenv('HOME'))
+	local current_dir = tostring(tab.active_pane.current_working_dir)
+	local HOME_DIR = string.format('file://%s', os.getenv('HOME'))
 
-  return current_dir == HOME_DIR and '.'
-      or string.gsub(current_dir, '(.*[/\\])(.*)', '%2')
+	return current_dir == HOME_DIR and '.' or string.gsub(current_dir, '(.*[/\\])(.*)', '%2')
 end
 
 local function get_process(tab)
-  local process_name = string.gsub(tab.active_pane.foreground_process_name, '(.*[/\\])(.*)', '%2')
+	local process_name = string.gsub(tab.active_pane.foreground_process_name, '(.*[/\\])(.*)', '%2')
 
-  return wezterm.format(
-    process_icons[process_name]
-    or { { Text = string.format('[%s]', process_name) } }
-  )
+	return wezterm.format(process_icons[process_name] or { {
+		Text = string.format('[%s]', process_name)
+	} })
 end
 
 wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
-    local has_unseen_output = false
-    if not tab.is_active then
-      for _, pane in ipairs(tab.panes) do
-        if pane.has_unseen_output then
-          has_unseen_output = true
-          break
-        end
-      end
-    end
+	local has_unseen_output = false
+	if not tab.is_active then
+		for _, pane in ipairs(tab.panes) do
+			if pane.has_unseen_output then
+				has_unseen_output = true
+				break
+			end
+		end
+	end
 
-    local title = string.format('%s. test %s ~ %s  ', tab.tab_index + 1, get_process(tab), get_current_working_dir(tab))
+	local tab_index = tab.tab_index + 1
+	local tab_process = get_process(tab)
+	local tab_cwd
 
-    if has_unseen_output then
-      return {
-        { Foreground = { Color = 'Orange' } },
-        { Text = title },
-      }
-    end
+	if tab_process == process_icons['ssh'] then
+		tab_cwd = wezterm.hostname()
+	else
+		tab_cwd = get_current_working_dir(tab)
+	end
 
-    return {
-      { Text = title },
-    }
-  end
-)
+	local title = string.format('%s. %s ~ %s  ', tab_index, tab_process, tab_cwd)
+
+	if has_unseen_output then
+		return { {
+			Foreground = {
+				Color = 'Orange'
+			}
+		}, {
+			Text = title
+		} }
+	end
+
+	return { {
+		Text = title
+	} }
+end)
 
 wezterm.on('update-right-status', function(window)
-  if not window:get_dimensions().is_full_screen then
-    window:set_right_status("")
-    return
-  end
+	if not window:get_dimensions().is_full_screen then
+		window:set_right_status("")
+		return
+	end
 
-  window:set_right_status(wezterm.format({
-    { Foreground = { Color = '#808080' } },
-    { Text = wezterm.strftime(' %R ') },
-  }))
+	window:set_right_status(wezterm.format({ {
+		Foreground = {
+			Color = '#808080'
+		}
+	}, {
+		Text = wezterm.strftime(' %R ')
+	} }))
 end)
 
 -- Fonts
@@ -175,7 +188,6 @@ config.font_size = 14
 config.window_frame = {
 	font = wezterm.font 'JetBrainsMono Nerd Font',
 	font_size = 14
-
 }
 
 wezterm.on('update-right-status', function(window, pane)
@@ -183,147 +195,200 @@ wezterm.on('update-right-status', function(window, pane)
 end)
 
 -- Keys
-config.leader = { key = 'Space', mods = 'CTRL|OPT', timeout_milliseconds = 2000 }
-config.keys = {
-	-- Workspaces
+config.leader = {
+	key = 'Space',
+	mods = 'CTRL|OPT',
+	timeout_milliseconds = 2000
+}
+config.keys = { -- Workspaces
 	{
 		key = '0',
 		mods = 'LEADER',
 		action = act.SwitchToWorkspace {
-			name = 'default',
-		},
-	},
-	{
-		key = '1',
-		mods = 'LEADER',
-		action = act.SwitchToWorkspace {
-			name = 'Qualityminds',
-			spawn = {
-				cwd = '/Users/lech/Repositories/Qualityminds/'
-			},
-		},
-	},
-	{
-		key = '2',
-		mods = 'LEADER',
-		action = act.SwitchToWorkspace {
-			name = 'Zeiss',
-			spawn = {
-				cwd = '/Users/lech/Repositories/Zeiss/'
-			},
-		},
-	},
-	{
+			name = 'default'
+		}
+	}, {
+	key = '1',
+	mods = 'LEADER',
+	action = act.SwitchToWorkspace {
+		name = 'Qualityminds',
+		spawn = {
+			cwd = '/Users/lech/Repositories/Qualityminds/'
+		}
+	}
+}, {
+	key = '2',
+	mods = 'LEADER',
+	action = act.SwitchToWorkspace {
+		name = 'Zeiss',
+		spawn = {
+			cwd = '/Users/lech/Repositories/Zeiss/'
+		}
+	}
+}, {
 	key = '3',
-		mods = 'LEADER',
-		action = act.SwitchToWorkspace {
-			name = 'DigitalTeammates',
-			spawn = {
-				cwd = '/Users/lech/Repositories/DigitalTeammates/'
-			},
-		},
-	},
-	{
-		key = '4',
-		mods = 'LEADER',
-		action = act.SwitchToWorkspace {
-			name = 'MunichRe',
-			spawn = {
-				cwd = '/Users/lech/Repositories/MunichRe/'
-			},
-		},
-	},
-	{
-		key = '5',
-		mods = 'LEADER',
-		action = act.SwitchToWorkspace {
-			name = 'Personal',
-			spawn = {
-				cwd = '/Users/lech/Repositories/Personal/'
-			},
-		},
-	},
-	{
-		key = 'w',
-		mods = 'LEADER',
-		action = wezterm.action.CloseCurrentPane { confirm = false },
-	},-- Splits
+	mods = 'LEADER',
+	action = act.SwitchToWorkspace {
+		name = 'DigitalTeammates',
+		spawn = {
+			cwd = '/Users/lech/Repositories/DigitalTeammates/'
+		}
+	}
+}, {
+	key = '4',
+	mods = 'LEADER',
+	action = act.SwitchToWorkspace {
+		name = 'MunichRe',
+		spawn = {
+			cwd = '/Users/lech/Repositories/MunichRe/'
+		}
+	}
+}, {
+	key = '5',
+	mods = 'LEADER',
+	action = act.SwitchToWorkspace {
+		name = 'Personal',
+		spawn = {
+			cwd = '/Users/lech/Repositories/Personal/'
+		}
+	}
+}, {
+	key = 'w',
+	mods = 'LEADER',
+	action = wezterm.action.CloseCurrentPane {
+		confirm = false
+	}
+}, -- Splits
 	{
 		key = 'x',
 		mods = 'LEADER',
-		action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-	},
-	{
-		key = 'v',
-		mods = 'LEADER',
-		action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-	},
-	{
-		key = 'f',
-		mods = 'CTRL|CMD',
-		action = wezterm.action.ToggleFullScreen,
-	},
-	{
-		key = 'r',
-		mods = 'LEADER',
-		action = act.ActivateKeyTable {
-			name = 'resize_pane',
-			one_shot = false,
-		},
-	},
-	{
-		key = 'a',
-		mods = 'LEADER',
-		action = act.ActivateKeyTable {
-			name = 'activate_pane',
-			timeout_milliseconds = 5000,
-		},
-	},
-	{
-		key = 'd',
-		mods = 'CMD',
-		action = wezterm.action.SplitPane {
-			direction = 'Next',
-			size = { Percent = 50 },
+		action = wezterm.action.SplitVertical {
+			domain = 'CurrentPaneDomain'
 		}
-	},
-	-- Resize pane
-	{ key = 'h', mods = 'OPT|SHIFT', action = act.AdjustPaneSize { 'Left', 1 } },
-	{ key = 'l', mods = 'OPT|SHIFT', action = act.AdjustPaneSize { 'Right', 1 } },
-	{ key = 'k', mods = 'OPT|SHIFT', action = act.AdjustPaneSize { 'Up', 1 } },
-	{ key = 'j', mods = 'OPT|SHIFT', action = act.AdjustPaneSize { 'Down', 1 } },
-	-- Swap pane
-	{ key = 'h', mods = 'OPT',       action = act.ActivatePaneDirection 'Left' },
-	{ key = 'l', mods = 'OPT',       action = act.ActivatePaneDirection 'Right' },
-	{ key = 'k', mods = 'OPT',       action = act.ActivatePaneDirection 'Up' },
-	{ key = 'j', mods = 'OPT',       action = act.ActivatePaneDirection 'Down' },
-}
+	}, {
+	key = 'v',
+	mods = 'LEADER',
+	action = wezterm.action.SplitHorizontal {
+		domain = 'CurrentPaneDomain'
+	}
+}, {
+	key = 'f',
+	mods = 'CTRL|CMD',
+	action = wezterm.action.ToggleFullScreen
+}, {
+	key = 'r',
+	mods = 'LEADER',
+	action = act.ActivateKeyTable {
+		name = 'resize_pane',
+		one_shot = false
+	}
+}, {
+	key = 'a',
+	mods = 'LEADER',
+	action = act.ActivateKeyTable {
+		name = 'activate_pane',
+		timeout_milliseconds = 5000
+	}
+}, {
+	key = 'd',
+	mods = 'CMD',
+	action = wezterm.action.SplitPane {
+		direction = 'Next',
+		size = {
+			Percent = 50
+		}
+	}
+}, -- Resize pane
+	{
+		key = 'h',
+		mods = 'OPT|SHIFT',
+		action = act.AdjustPaneSize { 'Left', 1 }
+	}, {
+	key = 'l',
+	mods = 'OPT|SHIFT',
+	action = act.AdjustPaneSize { 'Right', 1 }
+}, {
+	key = 'k',
+	mods = 'OPT|SHIFT',
+	action = act.AdjustPaneSize { 'Up', 1 }
+}, {
+	key = 'j',
+	mods = 'OPT|SHIFT',
+	action = act.AdjustPaneSize { 'Down', 1 }
+}, -- Swap pane
+	{
+		key = 'h',
+		mods = 'OPT',
+		action = act.ActivatePaneDirection 'Left'
+	}, {
+	key = 'l',
+	mods = 'OPT',
+	action = act.ActivatePaneDirection 'Right'
+}, {
+	key = 'k',
+	mods = 'OPT',
+	action = act.ActivatePaneDirection 'Up'
+}, {
+	key = 'j',
+	mods = 'OPT',
+	action = act.ActivatePaneDirection 'Down'
+} }
 
 config.key_tables = {
-	resize_pane = {
-		{ key = 'LeftArrow',  action = act.AdjustPaneSize { 'Left', 1 } },
-		{ key = 'h',          action = act.AdjustPaneSize { 'Left', 1 } },
-		{ key = 'RightArrow', action = act.AdjustPaneSize { 'Right', 1 } },
-		{ key = 'l',          action = act.AdjustPaneSize { 'Right', 1 } },
-		{ key = 'UpArrow',    action = act.AdjustPaneSize { 'Up', 1 } },
-		{ key = 'k',          action = act.AdjustPaneSize { 'Up', 1 } },
-		{ key = 'DownArrow',  action = act.AdjustPaneSize { 'Down', 1 } },
-		{ key = 'j',          action = act.AdjustPaneSize { 'Down', 1 } },
-
-		-- Cancel the mode by pressing escape
-		{ key = 'Escape',     action = 'PopKeyTable' },
-	},
-	activate_pane = {
-		{ key = 'LeftArrow',  action = act.ActivatePaneDirection 'Left' },
-		{ key = 'h',          action = act.ActivatePaneDirection 'Left' },
-		{ key = 'RightArrow', action = act.ActivatePaneDirection 'Right' },
-		{ key = 'l',          action = act.ActivatePaneDirection 'Right' },
-		{ key = 'UpArrow',    action = act.ActivatePaneDirection 'Up' },
-		{ key = 'k',          action = act.ActivatePaneDirection 'Up' },
-		{ key = 'DownArrow',  action = act.ActivatePaneDirection 'Down' },
-		{ key = 'j',          action = act.ActivatePaneDirection 'Down' },
-	},
+	resize_pane = { {
+		key = 'LeftArrow',
+		action = act.AdjustPaneSize { 'Left', 1 }
+	}, {
+		key = 'h',
+		action = act.AdjustPaneSize { 'Left', 1 }
+	}, {
+		key = 'RightArrow',
+		action = act.AdjustPaneSize { 'Right', 1 }
+	}, {
+		key = 'l',
+		action = act.AdjustPaneSize { 'Right', 1 }
+	}, {
+		key = 'UpArrow',
+		action = act.AdjustPaneSize { 'Up', 1 }
+	}, {
+		key = 'k',
+		action = act.AdjustPaneSize { 'Up', 1 }
+	}, {
+		key = 'DownArrow',
+		action = act.AdjustPaneSize { 'Down', 1 }
+	}, {
+		key = 'j',
+		action = act.AdjustPaneSize { 'Down', 1 }
+	},   -- Cancel the mode by pressing escape
+		{
+			key = 'Escape',
+			action = 'PopKeyTable'
+		} },
+	activate_pane = { {
+		key = 'LeftArrow',
+		action = act.ActivatePaneDirection 'Left'
+	}, {
+		key = 'h',
+		action = act.ActivatePaneDirection 'Left'
+	}, {
+		key = 'RightArrow',
+		action = act.ActivatePaneDirection 'Right'
+	}, {
+		key = 'l',
+		action = act.ActivatePaneDirection 'Right'
+	}, {
+		key = 'UpArrow',
+		action = act.ActivatePaneDirection 'Up'
+	}, {
+		key = 'k',
+		action = act.ActivatePaneDirection 'Up'
+	}, {
+		key = 'DownArrow',
+		action = act.ActivatePaneDirection 'Down'
+	}, {
+		key = 'j',
+		action = act.ActivatePaneDirection 'Down'
+	} }
 }
 
--- and finally, return the configuration to wezterm
 return config
