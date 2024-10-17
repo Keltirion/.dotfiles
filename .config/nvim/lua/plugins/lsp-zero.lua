573b06009d9d535854be18be35f2ffac7b7309aa
Keltrion <llonczynski@gmail.com>
2024-10-17 13:11:13 +0200

    Changes to lsp, disabled harpoon

--- .config/nvim/lua/plugins/lsp-zero.lua
+++ .config/nvim/lua/plugins/lsp-zero.lua
@@ -225,14 +225,14 @@ return {
 		require('lsp-zero').extend_cmp()
 		require('luasnip.loaders.from_vscode').lazy_load()
 
-		-- local luasnip = require('luasnip')
+		local luasnip = require('luasnip')
 		local cmp = require('cmp')
 
-		-- local has_words_before = function()
-		-- 	unpack = unpack or table.unpack
-		-- 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-		-- 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-		-- end
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
 
 		cmp.setup({
 			sources = {
@@ -252,30 +252,30 @@ return {
 				-- Ctrl+Space to trigger completion menu
 				['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
 
-				-- -- Super 'Tab'
-				-- ["<Tab>"] = cmp.mapping(function(fallback)
-				-- 	if cmp.visible() then
-				-- 		cmp.select_next_item()
-				-- 		-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-				-- 		-- they way you will only jump inside the snippet region
-				-- 	elseif luasnip.expand_or_jumpable() then
-				-- 		luasnip.expand_or_jump()
-				-- 	elseif has_words_before() then
-				-- 		cmp.complete()
-				-- 	else
-				-- 		fallback()
-				-- 	end
-				-- end, { "i", "s" }),
-				--
-				-- ["<S-Tab>"] = cmp.mapping(function(fallback)
-				-- 	if cmp.visible() then
-				-- 		cmp.select_prev_item()
-				-- 	elseif luasnip.jumpable(-1) then
-				-- 		luasnip.jump(-1)
-				-- 	else
-				-- 		fallback()
-				-- 	end
-				-- end, { "i", "s" }),
+				-- Super 'Tab'
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+						-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+						-- they way you will only jump inside the snippet region
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			},
 			formatting = lsp.cmp_format({ details = true })
 		})
